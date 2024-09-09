2f70bbf8838a0fb472bb8063067087766ecd8d9d
Anton Kolo <kvanton7@gmail.com>
2024-09-09 17:30:03 +0200

    Added personal information

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
     <nav className="navbar">
       <div className="container">
         <Link href="/" className="logo">
-          Lutfhi.
+          anton.kolo
         </Link>
 
         <ul className="nav-links">
